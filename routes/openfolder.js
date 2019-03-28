dae4c8bd1aa4533e03839b982ce92b59dd705604
Harshit Pandey <harshitpandey678@gmail.com>
2019-03-28 20:34:46 +0530

    navigate back

--- routes/openfolder.js
+++ routes/openfolder.js
@@ -6,13 +6,22 @@ var db      = require('../mysql_database/db');
 
 
 router.use(function(req,res){
+  var navback=1;
   //set user object here as well
       db.query("SELECT * FROM `directory` WHERE `parent` = '" + req.body.id + "'",function(err,result){
         if (err)throw err;
-        console.log(result);  
+        // console.log(result[0].parent);
+        
+        db.query("SELECT `parent` FROM `directory` WHERE  `id` = '" + result[0].parent + "'", function(err, response) {
+          setValue(response);
+        }) 
+        function setValue(response) {
+          navback= response;
+        } 
         res.render('dashboard',{
           res:result,
-          globalpname:req.body.id
+          globalpname:req.body.id,
+          navback: navback
         });
     }); 
 });
