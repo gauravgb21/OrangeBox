eec17319b6d2e0ded53b0ed3e367c645ae92d1b8
harshittpandey <harshitpandey678@gmail.com>
2019-03-29 14:52:41 +0530

    fixed dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,16 +7,17 @@ router.use(function(req,res){
   //set user object here as well
   db.query("SELECT * FROM `users` WHERE `username` = '" + req.user.username + "'",function(err,rows){
       if (err)throw err;
-      
+
       db.query("SELECT * FROM `directory` WHERE `parent` = '" + rows[0].root_id + "'",function(err,result){
         if (err)throw err;
         console.log(result);
+
         res.render('dashboard',{
         	res:result,
-          globalpname:result[0].parent
+            globalpname:rows[0].root_id
         });
-	  }); 
-	    
+	  });
+
   });
 });
 
